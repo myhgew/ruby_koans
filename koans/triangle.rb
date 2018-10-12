ff0640aedb2eade53ed86a8a679cd6264885692f
Yuhua Mai <yuhua.mai@gmail.com>
2018-10-12 10:52:10 -0700

    Finish about_triangle_project_2.rb

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,6 +15,14 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a <= 0 or b <= 0 or c <= 0
+  	raise TriangleError
+  end 
+
+  if a + b <= c or a + c <= b or b + c <= a
+  	raise TriangleError
+  end
+
   if a == b and b == c
   	return :equilateral
   elsif a != b and b != c and a != c
